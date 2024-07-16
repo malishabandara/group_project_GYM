57c16167cd6352abeb5bc1b3595aa2c2362c7f22
Malisha Bandara <121422631+malishabandara@users.noreply.github.com>
2024-07-16 20:01:32 +0530

    Delete config directory

--- config/color.js
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: "#C7F03C",
-  secondary: "#F9C8D7",
-  white: "#FFFFFF",
-  black: "#000000",
-  middle: "#656565",
-  buttonSecondary: "#E5E5E5",
-  text: "#F3F2F2",
-};
