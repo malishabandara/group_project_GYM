4852b73d6b9546be948656b7d3dbf253c881039a
Isuru lakmal <99isurulakmal@gmail.com>
2024-12-16 03:42:57 +0530

    update new dependencies

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,4 @@
-import { Stack, useRouter } from "expo-router";
+import { useRouter } from "expo-router";
 import { AuthProvider, useAuth } from "../context/AuthContext";
 import { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
@@ -6,6 +6,14 @@ import { Session } from "@supabase/supabase-js";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { getUserData } from "../services/userServices";
+
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+import registerScreen from "./registerScreen";
+import successScreen from "./successScreen";
+import weightScreen from "./weightScreen";
+import logRoute from "./logRoute";
+const Stack = createStackNavigator();
 
 const _layout = () => {
   return (
@@ -15,9 +23,10 @@ const _layout = () => {
   );
 };
 
-const MainLayout = () => {
+const MainLayout = (props) => {
   const { setAuth, setUserData } = useAuth();
   const router = useRouter();
+  const navigation = props.navigation;
 
   const [loaded] = useFonts({
     "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
@@ -59,95 +68,100 @@ const MainLayout = () => {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="Register" />
-      <Stack.Screen name="Login" />
-      <Stack.Screen name="index" />
-      <Stack.Screen name="registerScreen" />
-      <Stack.Screen name="SplashScreen" />
-      <Stack.Screen name="successScreen" />
-      <Stack.Screen name="Welcome" />
-      <Stack.Screen name="(main)/Home" />
-      <Stack.Screen name="weightScreen" />
-      <Stack.Screen name="logRoute" />
-      <Stack.Screen name="(admin_tabs)" />
-      <Stack.Screen
-        name="admin/MemberCard"
-        options={({ navigation }) => ({
-          headerShown: true,
-          headerTitle: "User Details",
-          headerBackTitle: "Back",
-        })}
-      />
-      <Stack.Screen
-        name="admin/ViewUserDetails"
-        options={({ navigation }) => ({
-          headerShown: true,
-          headerTitle: "Profiles",
-          headerBackTitle: "Back",
-          headerStyle: {
-            backgroundColor: "#764ABC",
-          },
-          headerTintColor: "#F8F9FB",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-          headerTitleAlign: "center",
-        })}
-      />
-      <Stack.Screen
-        name="admin/ViewSchedule"
-        options={({ navigation }) => ({
-          headerShown: true,
-          headerTitle: "Workouts",
-          headerBackTitle: "Back",
-          headerStyle: {
-            backgroundColor: "#764ABC",
-          },
-          headerTintColor: "#F8F9FB",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-          headerTitleAlign: "center",
-        })}
-      />
-      <Stack.Screen
-        name="admin/AddSchedule"
-        options={({ navigation }) => ({
-          headerShown: true,
-          headerTitle: "Add Schedule",
-          headerBackTitle: "Back",
-          headerStyle: {
-            backgroundColor: "#764ABC",
-          },
-          headerTintColor: "#F8F9FB",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-          headerTitleAlign: "center",
-        })}
-      />
-      <Stack.Screen
-        name="admin/meal_plans"
-        options={({ navigation }) => ({
-          headerShown: true,
-          headerTitle: "Meal Plans",
-          headerBackTitle: "Back",
-          headerStyle: {
-            backgroundColor: "#764ABC",
-          },
-          headerTintColor: "#F8F9FB",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-          headerTitleAlign: "center",
-        })}
-      />
-    </Stack>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Register" />
+        <Stack.Screen name="Login" />
+        <Stack.Screen name="index" />
+        <Stack.Screen name="registerScreen" />
+        <Stack.Screen name="SplashScreen" />
+        <Stack.Screen name="successScreen" />
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="(main)/Home" component={main / Home} />
+        <Stack.Screen name="weightScreen" component={weightScreen} />
+        <Stack.Screen name="logRoute" component={logRoute} />
+        <Stack.Screen name="(admin_tabs)" component={admin_tabs} />
+        <Stack.Screen
+          name="admin/MemberCard"
+          component={admin / MemberCard}
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTitle: "User Details",
+            headerBackTitle: "Back",
+          })}
+        />
+        <Stack.Screen
+          name="admin/ViewUserDetails"
+          component={admin / ViewUserDetails}
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTitle: "Profiles",
+            headerBackTitle: "Back",
+            headerStyle: {
+              backgroundColor: "#764ABC",
+            },
+            headerTintColor: "#F8F9FB",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center",
+          })}
+        />
+        <Stack.Screen
+          name="admin/ViewSchedule"
+          component={admin / ViewSchedule}
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTitle: "Workouts",
+            headerBackTitle: "Back",
+            headerStyle: {
+              backgroundColor: "#764ABC",
+            },
+            headerTintColor: "#F8F9FB",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center",
+          })}
+        />
+        <Stack.Screen
+          name="admin/AddSchedule"
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTitle: "Add Schedule",
+            headerBackTitle: "Back",
+            headerStyle: {
+              backgroundColor: "#764ABC",
+            },
+            headerTintColor: "#F8F9FB",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center",
+          })}
+        />
+        <Stack.Screen
+          name="admin/meal_plans"
+          options={({ navigation }) => ({
+            headerShown: true,
+            headerTitle: "Meal Plans",
+            headerBackTitle: "Back",
+            headerStyle: {
+              backgroundColor: "#764ABC",
+            },
+            headerTintColor: "#F8F9FB",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerTitleAlign: "center",
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
